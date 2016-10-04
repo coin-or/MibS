fdc4cb7453a68182568eda44662386dab4690a3b
Ted Ralphs <ted@lehigh.edu>
2016-10-04 10:08:59 -0400

    srandom can't be called on Windows, even with MinGW compiler

--- src/MibSConfig.hpp
+++ src/MibSConfig.hpp
@@ -33,8 +33,8 @@
 #ifndef MibSConfig_hpp_
 #define MibSConfig_hpp_
 
-#if defined(_MSC_VER) || defined (__MNO_CYGWIN)/* Different function call in
-                                                  Windows */
+#if defined(_MSC_VER) || defined(__MINGW32__)/* Different function call in 
+						Windows */
 #define SRANDOM(seed) srand(seed)
 #define RANDOM() rand()
 #else
