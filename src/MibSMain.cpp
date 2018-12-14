e83edf8875e593b03eff5eb181c4edded1ba241a
Sahar Tahernejad <sat214@lehigh.edu>
2018-12-13 23:06:57 -0500

    Showing help after typing mibs

--- src/MibSMain.cpp
+++ src/MibSMain.cpp
@@ -49,6 +49,33 @@ int main(int argc, char* argv[])
 #ifdef  COIN_HAS_MPI
         AlpsKnowledgeBrokerMPI broker(argc, argv, model);
 #else
+	if(argc == 1){
+	    std::cout 
+		            << "\n========================================================"
+		            << "\n========================================================"
+		            <<   "\nWelcome to MibS (Mixed Integer Bilevel Solver)"
+		            <<   "\nCopyright (C) 2007-2017 Lehigh University, Scott DeNegre, and Ted Ralphs."
+		            <<   "\nAll Rights Reserved."
+		            <<   "\nThis software is licensed under the Eclipse Public License. Please see"
+			    <<   "\naccompanying file for terms."
+		//<<   "\nVersion: " << MIBS_VERSION
+		//          <<   "\nBuild Date: " << __DATE__
+		//#ifdef DIP_SVN_REV
+		//          <<   "\nRevision Number: " << DIP_SVN_REV
+		//#endif
+		            << "\n========================================================"
+		            << "\n========================================================"
+		            << "\n";
+	    std::cerr       << "================================================" << std::endl
+		            << "Usage:" << std::endl
+		            << "1)./mibs -Alps_instance file.mps" << std::endl
+		            << "       -MibS_auxiliaryInfoFile aux_file.aux" << std::endl
+		            << "       --BlockFile /FilePath/ABC.block" << std::endl
+			    << "2)./mibs -param mibs.par" << std::endl
+		            << "================================================" << std::endl
+		            << std::endl;
+	    return 0;
+	}
         AlpsKnowledgeBrokerSerial broker(argc, argv, model);
 #endif
 
