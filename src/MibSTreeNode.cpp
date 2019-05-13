b93b8ca261a371ed333ecc2df9f91cfa89dce27f
Sahar Tahernejad <sat214@lehigh.edu>
2019-05-13 14:54:21 -0400

    Deleting writelp and writemps

--- src/MibSTreeNode.cpp
+++ src/MibSTreeNode.cpp
@@ -346,8 +346,6 @@ MibSTreeNode::process(bool isRoot, bool rampUp)
             getKnowledgeBroker()->tempTimer().start();
         }
 
-	model->solver()->writeLp("replModel");
-	model->solver()->writeMps("replModelMPS", "mps", model->solver()->getObjSense());
         lpStatus = static_cast<BlisLpStatus> (bound(model));
 
 	if (model->boundingPass_ == 1) {
