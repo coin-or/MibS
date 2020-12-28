447791da0e50be4e08ff420aaf8b198b1c1d8204
Yu Xie <yux616@lehigh.edu>
2020-12-27 21:56:28 -0500

    update plot related code

--- scripts/analyze/process.py
+++ scripts/analyze/process.py
@@ -6,7 +6,10 @@
 
 import sys, os, collections
 import pandas as pd
-import numpy as np 
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib 
+import itertools
 
 def parseOutput(outputDir, gaps, writeCSV = True):
     """
@@ -165,7 +168,6 @@ def processTable(df, gaps, displayCols):
         
         # for debug print
         # inst == 'miblp_20_20_50_0110_10_1' and print(rsltDict[inst])
-
     
     # convert dict to structured df
     #  change to formal column names?
@@ -176,19 +178,92 @@ def processTable(df, gaps, displayCols):
     # with open('ltx_tb1.txt', 'w') as file:
     #     file.write(df_forprint.to_latex())
     
-    # OPTION 2: for each displayCol, print a table
+    # OPTION 2: for each displayCol, print a table; using slicer indexing
     with open('ltx_tb2.txt', 'a') as file:
         for col in displayCols:
             file.write(df_forprint.loc[:, (slice(None), col)].to_latex())
 
-    # print(df_forprint.columns)
-    return
+    # OPTION 3: just process table, do not print latex table to file
+    # -- add a parameter for this
+
+    return df_forprint
+
+def plotSelected(df, gaps, plotCols):
+    """
+        Make some plots for given gaps and columns.
+        Use the measures for the smallest gap as 1.
+        Input:
+            df: (processed!) pandas dataframe from processTable
+            gaps: a list of int
+            plotCol: columns to make single plots
+    """
+    # markers = itertools.cycle(('X', '+', '.', 'o', '*')) 
+    # colors = itertools.cycle(('dodgerblue', 'slateblue', 'blueviolet', 'palevioletred','lightcoral',
+        # 'sandybrown', 'gold', 'yellowgreen', 'darkturquoise')) #'orange',
+    colors = ['dodgerblue', 'slateblue', 'blueviolet', 'palevioletred','lightcoral',
+        'sandybrown', 'gold', 'yellowgreen', 'darkturquoise']
+
+    # lambda function used to create new columns
+    def computeRatio(r):
+        # check unsolved cases
+        if isinstance(r[g], str) or isinstance(r[gaps[0]], str): # and 'text' in r[g]: 
+            return -1 
+        # check rounded-to-zero cases
+        elif r[gaps[0]] == 0 and r[g] == 0:
+            return 1
+        elif r[gaps[0]] == 0 and r[g] > 0:
+            return r[g]/0.001
+        elif r[g] == 0 and r[gaps[0]] > 0:
+            return 0.001/r[gaps[0]]
+        # otherwise, normal division
+        else:
+            return r[g]/r[gaps[0]]
+
+    # plot some bar chart for summarized result
+    for col in plotCols:
+        # get columns to print using cross-section then flatten column index
+        df_sub = df.xs(col, level='fields', axis=1, drop_level=True)
+        df_sub.columns = df_sub.columns.to_flat_index()
+ 
+        for g in gaps: # revesed order to drop cols? gaps[::-1]
+            # handle the unsolved cases
+            df_sub[str(g)+'_r'] = df_sub.apply(computeRatio, axis=1)
+
+        df_plot = df_sub.loc[df_sub[str(gaps[0])+'_r'] >= 0, [str(g)+'_r' for g in gaps]]
+        
+        # begin plotting
+        fig, ax = plt.subplots(1,1, figsize=(30,5)) #figsize=(7.5,5)
+        # obtain xlocs and shifts
+        xloc = np.arange(len(df_plot.index))
+        mid = int(len(gaps) / 2)
+        bar_width = 1/(len(gaps)+1)
+        for i,g in enumerate(gaps):
+            ax.bar(xloc+(i-mid)*(bar_width*0.6), df_plot[str(g)+'_r'], 
+                label='TargetGap ='+str(g), color=colors[i], width=bar_width, alpha=0.5)
+        # set other elements
+        ax.set_title("Performance Plot for "+ plotCols[col])
+        ax.set_ylabel("Percentage of (base?) Case")
+        ax.set_xlabel("Instances")
+        ax.legend(loc='upper right', ncol=len(gaps), fontsize='x-small')
+        ax.grid(axis='y',alpha=0.2)
+        ax.set_xticks(xloc)
+        ax.set_xticklabels(df_plot.index, rotation=90, fontsize='x-small')
+        l_xlim = xloc[0] - (mid+1)*bar_width
+        r_xlim = xloc[-1] + (mid+1)*bar_width
+        ax.set_xlim(l_xlim, r_xlim)
+        # set special ylims
+        'time' in col and ax.set_ylim(bottom=-1,top=5) # only for cpu_time/check_feas_time
+        'solved' in col and ax.set_ylim(bottom=-1,top=3) # only for vf/ub solved
+
+        fig.tight_layout()
+        fig.savefig("./performance/test_"+col, dpi=fig.dpi)
 
 def main():
 
-    # choose based on experiments
-    gaps = [25, 50]
-    # gaps = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+    # choose based on experiment results
+    gaps = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+    # gaps = [25, 50]
+    gaps = sorted(gaps)
 
     # read from output file directly or read from cache
     # ? may change to a more elegant way later...
@@ -205,17 +280,33 @@ def main():
         else:
             print('read from summary file')
     
-    # the columns that we want to print
-    displayCols = [
-        'cpu_time',
-        'chk_feas_time',
-        'vf_solved',
-        'ub_solved',
-        'objval_cost'
-    ]
+    # the columns that we want to process and print
+    displayCols = {
+        'cpu_time': 'CPU Search Time',
+        'chk_feas_time': 'Check Feasibility Time',
+        'vf_solved': 'Number of VF problem solved',
+        'ub_solved': 'Number of UB problem solved',
+        'objval_cost': 'Object Value'
+    }
 
     # print tables
-    processTable(df_r, gaps, displayCols)
+    df_proc = processTable(df_r, gaps, displayCols)
+
+    # make some plots
+    plotCols = {
+        'cpu_time': 'CPU Search Time',
+        'chk_feas_time': 'Check Feasibility Time',
+        'vf_solved': 'Number of VF problem solved',
+        'ub_solved': 'Number of UB problem solved',
+        'objval_cost': 'Object Value'
+    } # this is the same as the above list or a subset
+
+    if set(plotCols).issubset(set(displayCols)):
+        plotSelected(df_proc, gaps, plotCols)
+    else:
+        print('plotCols is not a subset of available columns. Try available ones.')
+        plotCols_sub = list(set(displayCols)&set(plotCols))
+        plotSelected(df_proc, gaps, plotCols_sub)
 
 if __name__ == "__main__":
     main()
