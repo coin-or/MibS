79fd9bd5360085f2b841ddb2743c826bcc68a3c2
Ted Ralphs <ted@lehigh.edu>
2021-10-26 16:41:51 -0400

    Turn down SYMPHONY verbosity

--- src/MibSCutGenerator.cpp
+++ src/MibSCutGenerator.cpp
@@ -1478,7 +1478,7 @@ MibSCutGenerator::findLowerLevelSolWatermelonIC(double *uselessIneqs, double *lo
 		//sym_set_int_param(env, "use_hot_starts", FALSE);
 		sym_set_dbl_param(env, "time_limit", remainingTime);
 		//sym_set_int_param(env, "do_primal_heuristic", FALSE);
-		sym_set_int_param(env, "verbosity", 10);
+		sym_set_int_param(env, "verbosity", -2);
 		//sym_set_int_param(env, "prep_level", -1);
 		sym_set_int_param(env, "max_active_nodes", maxThreadsLL);
 		//sym_set_int_param(env, "tighten_root_bounds", FALSE);
