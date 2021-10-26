1c6d7cd02d4ea9361495bbf43498a57b8583bc9f
Ted Ralphs <ted@lehigh.edu>
2021-10-26 16:37:35 -0400

    Make sure variable bounds on integer variables are integer and fix a few SYMPHONY settings

--- src/MibSCutGenerator.cpp
+++ src/MibSCutGenerator.cpp
@@ -1459,8 +1459,10 @@ MibSCutGenerator::findLowerLevelSolWatermelonIC(double *uselessIneqs, double *lo
     for(i = 0; i < lCols; i++){
 	colIndex = lColInd[i];
 	value = lpSol[colIndex];
-	nSolver->setColLower(i, origColLb[colIndex] - value);
-	nSolver->setColUpper(i, origColUb[colIndex] - value);
+	nSolver->setColLower(i, floor(origColLb[colIndex] - value +
+                                      localModel_->etol_));
+        nSolver->setColUpper(i, ceil(origColUb[colIndex] - value -
+                                     localModel_->etol_));
     }
 
     remainingTime = timeLimit - localModel_->broker_->subTreeTimer().getTime();
@@ -1473,15 +1475,15 @@ MibSCutGenerator::findLowerLevelSolWatermelonIC(double *uselessIneqs, double *lo
 #if COIN_HAS_SYMPHONY
 	        sym_environment *env = dynamic_cast<OsiSymSolverInterface *>
 		    (nSolver)->getSymphonyEnvironment();
-		sym_set_int_param(env, "use_hot_starts", FALSE);
+		//sym_set_int_param(env, "use_hot_starts", FALSE);
 		sym_set_dbl_param(env, "time_limit", remainingTime);
-		sym_set_int_param(env, "do_primal_heuristic", FALSE);
-		sym_set_int_param(env, "verbosity", -2);
-		sym_set_int_param(env, "prep_level", -1);
+		//sym_set_int_param(env, "do_primal_heuristic", FALSE);
+		sym_set_int_param(env, "verbosity", 10);
+		//sym_set_int_param(env, "prep_level", -1);
 		sym_set_int_param(env, "max_active_nodes", maxThreadsLL);
-		sym_set_int_param(env, "tighten_root_bounds", FALSE);
-		sym_set_int_param(env, "max_sp_size", 100);
-		sym_set_int_param(env, "do_reduced_cost_fixing", FALSE);
+		//sym_set_int_param(env, "tighten_root_bounds", FALSE);
+		//sym_set_int_param(env, "max_sp_size", 100);
+		//sym_set_int_param(env, "do_reduced_cost_fixing", FALSE);
 		if (whichCutsLL == 0){
 		    sym_set_int_param(env, "generate_cgl_cuts", FALSE);
 		}else{
@@ -1515,6 +1517,7 @@ MibSCutGenerator::findLowerLevelSolWatermelonIC(double *uselessIneqs, double *lo
 #endif
     }
 
+    nSolver->writeLp("water");
     nSolver->branchAndBound();
 
     if(((feasCheckSolver == "SYMPHONY") && (sym_is_time_limit_reached
