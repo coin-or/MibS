acac4840126ced332e55d4eeea6f088bafac493a
Ted Ralphs <ted@lehigh.edu>
2021-11-28 11:21:48 -0500

    Adding scripts for doing experiments using PBS

---
+++ scripts/analyze/myrun.py
@@ -0,0 +1,360 @@
+# Script to set up parameters for MibS instance path.
+# Used to run experiments for diffrent cuts 
+# Last edited by yux616
+# Apr 2020
+
+# Executable path and name
+pbsfile = '/home/ted/Projects/MibS/scripts/analyze/mibs_batch.pbs'
+
+# Instance path
+# Directory name and path containing test instances in .mps format
+# Keys are used to name subdirs in output dir
+instanceDirs = {
+    # 'MIBLP-XU': '/home/usr/coinbrew/MibS/testSets/BilevelLib/general/MIBLP-XU',
+    'IBLP-FIS': '/home/sat214/cutPaperMibS/dataPaper/iblpFis',
+    'INTERD-DEN': '/home/sat214/cutPaperMibS/dataPaper/interdDen',
+    'IBLP-DEN': '/home/sat214/cutPaperMibS/dataPaper/iblpDen',
+    'IBLP-ZHANG': '/home/sat214/MIBS/ozaltinData/convertedData/Testbed1/'
+}
+
+#versions = ['1.1', 'ib']
+versions = ['1.2-opt']
+
+# Output parent path
+outputDir = '/home/ted/Projects/MibS/output'
+
+# Name
+testname = 'mibs'
+
+# Set up senarios
+mibsParamsInputs = {}
+
+commonParams = {
+    'Alps_timeLimit': '3600',
+    'Blis_scaleConFactor': '10000000000',
+    'Blis_heurStrategy': '0',             # -2: root, -1: auto, 0: disable, any positive integer
+    'Blis_heurRound': '0',                # -2: root, -1: auto, 0: disable, any positive integer
+    'MibS_usePreprocessor': '0',          # -1: auto, 0: false, 1: true
+    'MibS_useLowerObjHeuristic': '0',     # -1: auto, 0: false, 1: true
+    'MibS_useObjCutHeuristic': '0',       # -1: auto, 0: false, 1: true
+    'MibS_useWSHeuristic': '0',           # -1: auto, 0: false, 1: true
+    'MibS_useGreedyHeuristic': '0',       # 0: false, 1: true
+    'MibS_objBoundStrategy': '0',         # 0: LL obj bound, 1: interdiction bound
+    'MibS_blisCutStrategy': '-1',         # -2: root, -1: auto, 0: disable, any positive integer
+#    'MibS_warmStartLL': '0',
+    'MibS_maxThreadsLL': '1',
+    'MibS_allowRemoveCut': '0',           # 0: false, 1: true
+    'MibS_whichCutsLL': '2',              # 0: no cuts, 1: gomory only, 2: all cuts
+    'MibS_doDualFixing': '0',
+#    'MibS_feasCheckSolver': 'SYMPHONY',
+    'MibS_solveSecondLevelWhenXYVarsInt': '1',
+    'MibS_solveSecondLevelWhenXVarsInt': '0',
+    'MibS_solveSecondLevelWhenLVarsFixed': '1',
+    'MibS_computeBestUBWhenXVarsInt': '0',
+    'MibS_computeBestUBWhenLVarsInt': '0',
+    'MibS_computeBestUBWhenLVarsFixed': '1',
+    'MibS_useLinkingSolutionPool': '1',
+}    
+
+# mibsParamsInputs['default'] = {
+#     'MibS_branchStrategy': '1',           # 0: fractional, 1: linking
+# }
+
+# mibsParamsInputs['default-frac'] = {
+#     'MibS_branchStrategy': '0',           # 0: fractional, 1: linking
+# }
+
+# mibsParamsInputs['noGood+type1+pureInteger'] = {
+#     'MibS_turnOffOtherCuts': '1',
+#     'MibS_useGeneralNoGoodCut':'1',
+#     'MibS_useTypeIC': '1',
+#     'MibS_bilevelFreeSetTypeIC': '0',     # 0: Intersection Cut Type I; 1: Intersection Cut Type II; only valid when use intersectionCutTypeIC
+#     'MibS_usePureIntegerCut':'1',
+#     'MibS_branchStrategy': '1',           # 0: fractional, 1: linking
+#     'MibS_solveSecondLevelWhenLVarsInt': '1',
+# }
+
+# mibsParamsInputs['watermelon+type1+incObj-frac'] = {
+#     'MibS_turnOffOtherCuts': '1',
+#     'MibS_useIncObjCut':'1',
+#     'MibS_useTypeWatermelon': '1',
+#     'MibS_useTypeIC': '1',
+#     'MibS_bilevelFreeSetTypeIC': '0',     # 0: Intersection Cut Type I; 1: Intersection Cut Type II; only valid when use intersectionCutTypeIC
+#     'MibS_branchStrategy': '0',           # 0: fractional, 1: linking
+#     'MibS_solveSecondLevelWhenLVarsInt': '0',
+# }
+
+# mibsParamsInputs['watermelon+type1+incObj-frac-LV'] = {
+#     'MibS_turnOffOtherCuts': '1',
+#     'MibS_useIncObjCut':'1',
+#     'MibS_useTypeWatermelon': '1',
+#     'MibS_useTypeIC': '1',
+#     'MibS_bilevelFreeSetTypeIC': '0',     # 0: Intersection Cut Type I; 1: Intersection Cut Type II; only valid when use intersectionCutTypeIC
+#     'MibS_branchStrategy': '0',           # 0: fractional, 1: linking
+#     'MibS_solveSecondLevelWhenLVarsInt': '1',
+# }
+
+# mibsParamsInputs['watermelon+type1-frac'] = {
+#     'MibS_turnOffOtherCuts': '1',
+#     'MibS_useTypeWatermelon': '1',
+#     'MibS_useTypeIC': '1',
+#     'MibS_bilevelFreeSetTypeIC': '0',     # 0: Intersection Cut Type I; 1: Intersection Cut Type II; only valid when use intersectionCutTypeIC
+#     'MibS_branchStrategy': '0',           # 0: fractional, 1: linking
+#     'MibS_solveSecondLevelWhenLVarsInt': '0',
+# }
+
+# mibsParamsInputs['watermelon+type1-frac-LV'] = {
+#     'MibS_turnOffOtherCuts': '1',
+#     'MibS_useTypeWatermelon': '1',
+#     'MibS_useTypeIC': '1',
+#     'MibS_bilevelFreeSetTypeIC': '0',     # 0: Intersection Cut Type I; 1: Intersection Cut Type II; only valid when use intersectionCutTypeIC
+#     'MibS_branchStrategy': '0',           # 0: fractional, 1: linking
+#     'MibS_solveSecondLevelWhenLVarsInt': '1',
+# }
+
+# mibsParamsInputs['benders'] = {
+#     'MibS_turnOffOtherCuts': '1',
+#     'MibS_useBendersCut':'1',
+#     'MibS_branchStrategy': '1',           # 0: fractional, 1: linking
+#     'MibS_solveSecondLevelWhenLVarsInt': '1',
+# }
+
+# mibsParamsInputs['benders-frac'] = {
+#     'MibS_turnOffOtherCuts': '1',
+#     'MibS_useBendersCut':'1',
+#     'MibS_branchStrategy': '0',           # 0: fractional, 1: linking
+# }
+
+# mibsParamsInputs['benders-cplex'] = {
+#     'MibS_turnOffOtherCuts': '1',
+#     'MibS_useBendersCut':'1',
+#     'MibS_branchStrategy': '1',           # 0: fractional, 1: linking
+#     'MibS_solveSecondLevelWhenLVarsInt': '1',
+#     'MibS_feasCheckSolver' : 'CPLEX'
+# }
+
+# mibsParamsInputs['benders-frac-cplex'] = {
+#     'MibS_turnOffOtherCuts': '1',
+#     'MibS_useBendersCut':'1',
+#     'MibS_branchStrategy': '0',           # 0: fractional, 1: linking
+#     'MibS_feasCheckSolver' : 'CPLEX'
+# }
+
+# mibsParamsInputs['incObj'] = {
+#     'MibS_turnOffOtherCuts': '1',
+#     'MibS_useIncObjCut':'1',
+#     'MibS_branchStrategy': '1',           # 0: fractional, 1: linking
+#     'MibS_solveSecondLevelWhenLVarsInt': '1',
+# }
+
+# mibsParamsInputs['incObj-frac'] = {
+#     'MibS_turnOffOtherCuts': '1',
+#     'MibS_useIncObjCut':'1',
+#     'MibS_branchStrategy': '0',           # 0: fractional, 1: linking
+#     'MibS_solveSecondLevelWhenLVarsInt': '0',
+# }
+
+# mibsParamsInputs['genNoGood'] = {
+#     'MibS_turnOffOtherCuts': '1',
+#     'MibS_useGeneralNoGoodCut':'1',
+#     'MibS_branchStrategy': '1',           # 0: fractional, 1: linking
+#     'MibS_solveSecondLevelWhenLVarsInt': '1',           # 0: fractional, 1: linking
+# }
+
+# mibsParamsInputs['genNoGood-frac'] = {
+#     'MibS_turnOffOtherCuts': '1',
+#     'MibS_useGeneralNoGoodCut':'1',
+#     'MibS_branchStrategy': '0',           # 0: fractional, 1: linking
+#     'MibS_solveSecondLevelWhenLVarsInt': '0',           # 0: fractional, 1: linking
+# }
+
+# mibsParamsInputs['watermelon'] = {
+#     'MibS_turnOffOtherCuts': '1',
+#     'MibS_useTypeWatermelon': '1',
+#     'MibS_branchStrategy': '1',           # 0: fractional, 1: linking
+#     'MibS_solveSecondLevelWhenLVarsInt': '0',           # 0: fractional, 1: linking
+# }
+
+# mibsParamsInputs['watermelon-frac'] = {
+#     'MibS_turnOffOtherCuts': '1',
+#     'MibS_useTypeWatermelon': '1',
+#     'MibS_branchStrategy': '0',           # 0: fractional, 1: linking
+#     'MibS_solveSecondLevelWhenLVarsInt': '0',           # 0: fractional, 1: linking
+# }
+
+# mibsParamsInputs['watermelon-frac-LV'] = {
+#     'MibS_turnOffOtherCuts': '1',
+#     'MibS_useTypeWatermelon': '1',
+#     'MibS_branchStrategy': '0',           # 0: fractional, 1: linking
+#     'MibS_solveSecondLevelWhenLVarsInt': '1',           # 0: fractional, 1: linking
+# }
+
+# mibsParamsInputs['fracWatermelon'] = {
+#     'MibS_turnOffOtherCuts': '1',
+#     'MibS_useTypeWatermelon': '1',
+#     'MibS_useFractionalCuts': '1',
+#     'MibS_branchStrategy': '1',           # 0: fractional, 1: linking
+#     'MibS_solveSecondLevelWhenLVarsInt': '0',           # 0: fractional, 1: linking
+# }
+
+# mibsParamsInputs['fracWatermelon-frac'] = {
+#     'MibS_turnOffOtherCuts': '1',
+#     'MibS_useTypeWatermelon': '1',
+#     'MibS_useFractionalCuts': '1',
+#     'MibS_branchStrategy': '0',           # 0: fractional, 1: linking
+#     'MibS_solveSecondLevelWhenLVarsInt': '0',           # 0: fractional, 1: linking
+# }
+
+# mibsParamsInputs['fracWatermelon-frac-cplex'] = {
+#     'MibS_turnOffOtherCuts': '1',
+#     'MibS_useTypeWatermelon': '1',
+#     'MibS_useFractionalCuts': '1',
+#     'MibS_branchStrategy': '0',           # 0: fractional, 1: linking
+#     'MibS_solveSecondLevelWhenLVarsInt': '0',           # 0: fractional, 1: linking
+#     'MibS_feasCheckSolver' : 'CPLEX'
+# }
+
+# mibsParamsInputs['fracWatermelon-frac-cplex-d10'] = {
+#     'MibS_turnOffOtherCuts': '1',
+#     'MibS_useTypeWatermelon': '1',
+#     'MibS_useFractionalCuts': '1',
+#     'MibS_branchStrategy': '0',           # 0: fractional, 1: linking
+#     'MibS_solveSecondLevelWhenLVarsInt': '0',           # 0: fractional, 1: linking
+#     'MibS_feasCheckSolver' : 'CPLEX',
+#     'MibS_maxCutDepth' : '10'
+# }
+
+# mibsParamsInputs['fracWatermelon-frac-cplex-d20'] = {
+#     'MibS_turnOffOtherCuts': '1',
+#     'MibS_useTypeWatermelon': '1',
+#     'MibS_useFractionalCuts': '1',
+#     'MibS_branchStrategy': '0',           # 0: fractional, 1: linking
+#     'MibS_solveSecondLevelWhenLVarsInt': '0',           # 0: fractional, 1: linking
+#     'MibS_feasCheckSolver' : 'CPLEX',
+#     'MibS_maxCutDepth' : '20'
+# }
+
+# mibsParamsInputs['type1'] = {
+#     'MibS_turnOffOtherCuts': '1',
+#     'MibS_useTypeIC': '1',
+#     'MibS_bilevelFreeSetTypeIC': '0',     # 0: Intersection Cut Type I; 1: Intersection Cut Type II; only valid when use intersectionCutTypeIC
+#     'MibS_branchStrategy': '1',           # 0: fractional, 1: linking
+#     'MibS_solveSecondLevelWhenLVarsInt': '1',           # 0: fractional, 1: linking
+# }
+
+# mibsParamsInputs['type1-WS'] = {
+#     'MibS_turnOffOtherCuts': '1',
+#     'MibS_useTypeIC': '1',
+#     'MibS_bilevelFreeSetTypeIC': '0',     # 0: Intersection Cut Type I; 1: Intersection Cut Type II; only valid when use intersectionCutTypeIC
+#     'MibS_branchStrategy': '1',           # 0: fractional, 1: linking
+#     'MibS_solveSecondLevelWhenLVarsInt': '1',           # 0: fractional, 1: linking
+#     'MibS_warmStartLL': '1'
+# }
+
+# mibsParamsInputs['type1-cplex'] = {
+#     'MibS_turnOffOtherCuts': '1',
+#     'MibS_useTypeIC': '1',
+#     'MibS_bilevelFreeSetTypeIC': '0',     # 0: Intersection Cut Type I; 1: Intersection Cut Type II; only valid when use intersectionCutTypeIC
+#     'MibS_branchStrategy': '1',           # 0: fractional, 1: linking
+#     'MibS_solveSecondLevelWhenLVarsInt': '1',           # 0: fractional, 1: linking
+#     'MibS_feasCheckSolver' : 'CPLEX'
+# }
+
+# mibsParamsInputs['fracType1-frac-cplex'] = {
+#     'MibS_turnOffOtherCuts': '1',
+#     'MibS_useTypeIC': '1',
+#     'MibS_bilevelFreeSetTypeIC': '0',     # 0: Intersection Cut Type I; 1: Intersection Cut Type II; only valid when use intersectionCutTypeIC
+#     'MibS_branchStrategy': '1',           # 0: fractional, 1: linking
+#     'MibS_solveSecondLevelEveryIteration': '1',           # 0: fractional, 1: linking
+#     'MibS_feasCheckSolver' : 'CPLEX'
+# }
+
+# mibsParamsInputs['fracType1-frac-cplex-d10'] = {
+#     'MibS_turnOffOtherCuts': '1',
+#     'MibS_useTypeIC': '1',
+#     'MibS_bilevelFreeSetTypeIC': '0',     # 0: Intersection Cut Type I; 1: Intersection Cut Type II; only valid when use intersectionCutTypeIC
+#     'MibS_branchStrategy': '1',           # 0: fractional, 1: linking
+#     'MibS_solveSecondLevelEveryIteration': '1',           # 0: fractional, 1: linking
+#     'MibS_feasCheckSolver' : 'CPLEX',
+#     'MibS_maxCutDepth' : '10'
+# }
+
+# mibsParamsInputs['fracType1-frac-cplex-d20'] = {
+#     'MibS_turnOffOtherCuts': '1',
+#     'MibS_useTypeIC': '1',
+#     'MibS_bilevelFreeSetTypeIC': '0',     # 0: Intersection Cut Type I; 1: Intersection Cut Type II; only valid when use intersectionCutTypeIC
+#     'MibS_branchStrategy': '1',           # 0: fractional, 1: linking
+#     'MibS_solveSecondLevelEveryIteration': '1',           # 0: fractional, 1: linking
+#     'MibS_feasCheckSolver' : 'CPLEX',
+#     'MibS_maxCutDepth' : '20'
+# }
+
+# mibsParamsInputs['type1-frac'] = {
+#     'MibS_turnOffOtherCuts': '1',
+#     'MibS_useTypeIC': '1',
+#     'MibS_bilevelFreeSetTypeIC': '0',     # 0: Intersection Cut Type I; 1: Intersection Cut Type II; only valid when use intersectionCutTypeIC
+#     'MibS_branchStrategy': '0',           # 0: fractional, 1: linking
+#     'MibS_solveSecondLevelWhenLVarsInt': '0',           # 0: fractional, 1: linking
+# }
+
+# mibsParamsInputs['type2'] = {
+#     'MibS_turnOffOtherCuts': '1',
+#     'MibS_useTypeIC': '1',
+#     'MibS_bilevelFreeSetTypeIC': '1',     # 0: Intersection Cut Type I; 1: Intersection Cut Type II; only valid when use intersectionCutTypeIC
+#     'MibS_branchStrategy': '1',           # 0: fractional, 1: linking
+#     'MibS_solveSecondLevelWhenLVarsInt': '0',           # 0: fractional, 1: linking
+# }
+
+# mibsParamsInputs['type2-frac'] = {
+#     'MibS_turnOffOtherCuts': '1',
+#     'MibS_useTypeIC': '1',
+#     'MibS_bilevelFreeSetTypeIC': '1',     # 0: Intersection Cut Type I; 1: Intersection Cut Type II; only valid when use intersectionCutTypeIC
+#     'MibS_branchStrategy': '0',           # 0: fractional, 1: linking
+#     'MibS_solveSecondLevelWhenLVarsInt': '0',           # 0: fractional, 1: linking
+# }
+
+# mibsParamsInputs['hyper'] = {
+#     'MibS_turnOffOtherCuts': '1',
+#     'MibS_useTypeHypercubeIC': '1',
+#     'MibS_branchStrategy': '1',           # 0: fractional, 1: linking
+#     'MibS_solveSecondLevelWhenLVarsInt': '1',           # 0: fractional, 1: linking
+# }
+
+# mibsParamsInputs['hyper-frac'] = {
+#     'MibS_turnOffOtherCuts': '1',
+#     'MibS_useTypeHypercubeIC': '1',
+#     'MibS_branchStrategy': '0',           # 0: fractional, 1: linking
+#     'MibS_solveSecondLevelWhenLVarsInt': '0',           # 0: fractional, 1: linking
+# }
+
+# mibsParamsInputs['pureInteger'] = {
+#     'MibS_turnOffOtherCuts': '1',
+#     'MibS_usePureIntegerCut':'1',
+#     'MibS_branchStrategy': '1',           # 0: fractional, 1: linking
+#     'MibS_solveSecondLevelWhenLVarsInt': '0',           # 0: fractional, 1: linking
+# }
+
+# mibsParamsInputs['pureInteger-frac'] = {
+#     'MibS_turnOffOtherCuts': '1',
+#     'MibS_usePureIntegerCut':'1',
+#     'MibS_branchStrategy': '0',           # 0: fractional, 1: linking
+#     'MibS_solveSecondLevelWhenLVarsInt': '0',           # 0: fractional, 1: linking
+# }
+
+# mibsParamsInputs['noCut'] = {
+#      'MibS_branchStrategy': '1',           # 0: fractional, 1: linking
+#      'MibS_cutStrategy': '0'
+# }
+
+mibsParamsInputs['noCut-cplex'] = {
+     'MibS_branchStrategy': '1',           # 0: fractional, 1: linking
+     'MibS_cutStrategy': '0',
+     'MibS_feasCheckSolver' : 'CPLEX',
+}
+
+# mibsParamsInputs['noCut-WS'] = {
+#     'MibS_branchStrategy': '1',           # 0: fractional, 1: linking
+#     'MibS_cutStrategy': '0',
+#     'MibS_warmStartLL': '1'
+# }
+
---
+++ scripts/analyze/run.py
@@ -0,0 +1,220 @@
+# Script to run MibS with different SL target gap.
+# The script may produce auxiliary folder/files in run directory. 
+# Last edited by yux616
+# Apr 2020 
+# Script path:  /MibS/scripts/analyze
+# Some os function requires Python 3.5+
+
+# add arg parser later
+
+import sys, os, collections
+import shutil, subprocess
+# from types import FrameType
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+#import matplotlib 
+#import itertools
+
+# from runparams import exe, instanceDirs, outputDir, mibsParamsInputs, pbsfile
+# from runparams_cuts import exe, instanceDirs, outputDir, mibsParamsInputs, pbsfile
+from myrun import instanceDirs, outputDir, mibsParamsInputs, pbsfile, testname, versions, commonParams
+
+def runExperiments(exe, instPaths, outDir, versions, params, gaps=[]):
+    """
+        Use to run experiments on local machine. 
+    """
+    writeParams = False
+
+    # set up output directories
+    # use hierarchy:  outDir/version/param_scenario_name/testset_name/
+    for v in versions:
+        for scenario in params:
+            # print(scenario)
+            currpath = os.path.join(outDir, v, scenario)
+            if not os.path.exists(currpath):
+                os.mkdir(currpath)
+            for testset in instPaths:
+                currsubpath1 = os.path.join(currpath, testset)
+                if not os.path.exists(currsubpath1):
+                    os.mkdir(currsubpath1)
+                if gaps: 
+                    for g in gaps:
+                        currsubpath2 = os.path.join(currsubpath1,'BR' + str(g) + 'Output')
+                        if not os.path.exists(currsubpath2):
+                            os.mkdir(currsubpath2)
+   
+    # if choose to write params into files
+    if writeParams:
+        cwd = os.getcwd()
+        parampath = os.path.join(cwd, '../parameters')
+        if not os.path.exists(parampath):
+            os.mkdir(parampath)
+        
+        for scenario in params:
+            # for this scenario, make a parameter file and save to specified directory
+            paramsubpath1 = os.path.join(parampath, scenario)
+            if not os.path.exists(paramsubpath1):
+                os.mkdir(paramsubpath1)
+            os.chdir(paramsubpath1)
+            file = open(scenario+'.par', 'w')
+            for k, v in params[scenario].items():
+                file.write(k + ' ' + v + '\n')
+            file.close()
+            if gaps:
+                for g in gaps:
+                    src = scenario + '.par'
+                    dst = scenario + '_g'+ str(g) +'.par'
+                    shutil.copyfile(src, dst)
+                    file = open(dst, 'a')
+                    file.write('MibS_slTargetGap ' + str(g) + '\n')
+                    file.close()
+        
+            # run experiments use modified parameter file      
+            for testset in instPaths:
+                if gaps: 
+                    for g in gaps:
+                        paramfile = os.path.join(paramsubpath1, scenario + '_g'+ str(g) +'.par')
+                        outsubpath = os.path.join(outDir, scenario, testset,'BR' + str(g) + 'Output')
+                        os.chdir(outsubpath)   
+                        with os.scandir(instPaths[testset]) as inst_it: 
+                            for instance in inst_it:
+                                if instance.name.endswith('.mps'):
+                                    outname = instance.name[:-4]+'.out'
+                                    outfile = open(outname,'w')
+                                    subprocess.run([exe, "-param", paramfile,
+                                                    "-Alps_instance", instance.path,
+                                                    "-MibS_auxiliaryInfoFile", instance.path[:-4]+".aux",
+                                                    ],
+                                                    stdout=outfile)
+                                    outfile.close()
+                                    print('Complete {} with gap {}'.format(instance.name, g))
+                else:
+                    pass # add alternative call information
+        # remove paramter files created earlier?   
+    else:
+        # run experiments use command line paramters
+        for v in versions:
+            for scenario in params:
+                for testset in instPaths:
+                    if gaps: 
+                        for g in gaps:
+                            paramcmd = ' -'.join(' '.join(_) for _ in params[scenario].items())
+                            paramcmd = '-' + paramcmd + ' -MibS_slTargetGap ' + str(g)
+                            # print(paramcmd)
+                            outsubpath = os.path.join(outDir, v, scenario, testset,'BR' + str(g) + 'Output')
+                            os.chdir(outsubpath)   
+                            with os.scandir(instPaths[testset]) as inst_it: 
+                                for instance in inst_it:
+                                    if instance.name.endswith('.mps'):
+                                        outname = instance.name[:-4]+'.out'
+                                        outfile = open(outname,'w')
+                                        subprocess.run([exe,
+                                                        "-Alps_instance", instance.path,
+                                                        "-MibS_auxiliaryInfoFile", instance.path[:-4]+".aux",
+                                                        paramcmd,
+                                                    ],
+                                                       stdout=outfile)
+                                        outfile.close()
+                                        print('Complete {} with gap {}'.format(instance.name, g))
+                    else:
+                        pass # add alternative call information
+    return
+
+def runExperimentsPBS(instPaths, outDir, versions, params, pbsfile, gaps=[]):
+    """
+        Use to submit batch jobs via qsub. 
+    """
+    # set up output directories
+    # use hierarchy:  outDir/version/param_scenario_name/testset_name
+    for v in versions:
+        for scenario in params:
+            for testset in instPaths:
+                if gaps: 
+                    for g in gaps:
+                        currsubpath = os.path.join(outDir, v, scenario, testset, 'BR' + str(g) + 'Output')
+                        if not os.path.exists(currsubpath):
+                            os.makedirs(currsubpath)
+                        else:
+                            shutil.rmtree(currsubpath, ignore_errors=True)
+                            os.makedirs(currsubpath, exist_ok=True)      
+                else:
+                    currsubpath = os.path.join(outDir, v, scenario, testset)
+                    if not os.path.exists(currsubpath):
+                        os.makedirs(currsubpath)
+                    else:
+                        print('Directory already exists! Ignoring...')
+                        #sys.exit()
+                        #shutil.rmtree(currsubpath, ignore_errors=True)
+                        #os.makedirs(currsubpath, exist_ok=True)  
+
+    # submit experiments use command line paramters
+    # see .pbs file anc comments for job submission arguments
+    for v in versions:
+        if v == 'ib':
+            exe = '/home/ted/Projects/build-mibs-debug-no-cplex/bin/mibs'
+        if v == '1.1':
+            exe = '/home/ted/tmp/COIN/build-mibs-1.1-debug/bin/mibs'
+        if v == '1.2-opt':
+            #exe = '/home/ted/Projects/build-mibs-1.2/bin/mibs'
+            exe = '/home/ted/Projects/build-mibs-cplex-1.2/bin/mibs'
+            
+        for scenario in params:
+            for testset in instPaths:
+                paramcmd = ' -'.join(' '.join(_) for _ in params[scenario].items())
+                if gaps: 
+                    for g in gaps:
+                        paramcmd_g = '-' + paramcmd + ' -MibS_slTargetGap ' + str(g)
+                        # print(paramcmd)
+                        outsubpath = os.path.join(outDir, v, scenario, testset,'BR' + str(g) + 'Output')
+                        os.chdir(outsubpath)   
+                        with os.scandir(instPaths[testset]) as inst_it: 
+                            for instance in inst_it:
+                                if instance.name.endswith('.mps'):
+                                    outfile = os.path.join(outsubpath, instance.name[:-4]+'.out')
+                                    errfile = os.path.join(outsubpath, instance.name[:-4]+'.err')
+                                    subprocess.run(["qsub", "-v", 
+                                                    "EXECUTABLE="+exe+","
+                                                    +"INSTANCENAME="+instance.path+","
+                                                    +"AUXNAME="+instance.path[:-4]+".aux"+","
+                                                    +"PARAMARG="+paramcmd_g,
+                                                    "-o", outfile,
+                                                    "-e", errfile,
+                                                    "-N", testname,
+                                                    pbsfile])
+                else:
+                    outsubpath = os.path.join(outDir, v, scenario, testset)
+                    os.chdir(outsubpath)   
+                    with os.scandir(instPaths[testset]) as inst_it: 
+                        for instance in inst_it:
+                            if instance.name.endswith('.mps'):
+                                outfile = os.path.join(outsubpath, instance.name[:-4]+'.out')
+                                errfile = os.path.join(outsubpath, instance.name[:-4]+'.err')
+                                if not os.path.exists(outfile):
+                                    subprocess.run(["qsub", "-v", 
+                                                    "EXECUTABLE="+exe+","
+                                                    +"INSTANCENAME="+instance.path+","
+                                                    +"AUXNAME="+instance.path[:-4]+".aux"+","
+                                                    +"PARAMARG="+paramcmd,
+                                                    "-o", outfile,
+                                                    "-e", errfile,
+                                                    "-N", testname,
+                                                    pbsfile])
+                                else:
+                                    print("File", outfile, "exists!")
+    return                    
+
+if __name__ == "__main__":
+
+    for t in mibsParamsInputs:
+        mibsParamsInputs[t].update(commonParams)
+
+    ######################### Run Experimests #########################
+    # local: provide paths in runparams.py
+    # runExperiments(exe, instanceDirs, outputDir, versions, mibsParamsInputs, gaps)
+    
+    # using pbs file: provide paths in runparams.py
+    runExperimentsPBS(instanceDirs, outputDir, versions, mibsParamsInputs, pbsfile, gaps)
+    
+
+
