3daa123b8cb6ef50977a47565786c5f7ddc9e472
Yu <yux616@lehigh.edu>
2023-02-10 21:35:24 -0500

    update readAux to include namebased
    (cherry picked from commit 79e81c0bab5e8206774ece8baf8ed80d8c91b43b)
    Signed-off-by: Yu <yux616@lehigh.edu>

--- src/MibSModel.cpp
+++ src/MibSModel.cpp
@@ -327,12 +327,12 @@ MibSModel::readAuxiliaryData(const CoinPackedMatrix& rowMatrix,
      int lowerColNum(0), lowerRowNum(0);
 
      while (data_stream >> key){
-	 if(key == "N"){
+	 if((key == "N")||(key == "@NUMVARS")){
 	     data_stream >> iValue;
 	     setLowerDim(iValue);
 	     setTruncLowerDim(iValue);
 	 }
-         else if(key == "M"){
+         else if((key == "M")||(key == "@NUMCONSTRS")){
 	     data_stream >> iValue;
 	     setLowerRowNum(iValue);
 	     setTruncLowerRowNum(iValue);
@@ -391,6 +391,11 @@ MibSModel::readAuxiliaryData(const CoinPackedMatrix& rowMatrix,
 	     data_stream >> dValue;
 	     lowerObjSense_ = dValue; //1 min; -1 max
 	 }
+    else if(key == "@OBJSENSE"){
+       data_stream >> cValue;
+       // YX: if not MAX, assume MIN
+       lowerObjSense_ = (cValue.compare("MAX") == 0)? -1: 1; 
+    }
 	 else if(key == "IC"){
 	     if(!getInterdictCost()){
 		 //FIXME: ALLOW MORE THAN ONE ROW
@@ -427,13 +432,13 @@ MibSModel::readAuxiliaryData(const CoinPackedMatrix& rowMatrix,
 		     std::cout << cValue << " does not belong to the list of variables." << std::endl;
 		 }
 	         else{
-		     lowerObjCoeffs_[pos] = dValue;
+		     lowerObjCoeffs_[i] = dValue;
 		     lowerColInd_[i] = pos;
 		 }
 	         pos = -1;
 	     }
 	 }
-         else if(key == "@CONSTSBEGIN"){
+         else if(key == "@CONSTRSBEGIN"){ // YX: keyword changed
 	     pos = -1;
 	     lowerRowNum = getLowerRowNum();
 	     if(!getLowerRowInd())
@@ -990,14 +995,14 @@ std::string cmtStr)
    output->puts(line.c_str());
 
    // YX: write vars with objcoeff; then constraints;
-   line = "@VARBEGIN";
+   line = "@VARSBEGIN";
    for(i = 0; i < lowerDim_; ++i){
       j = lowerColInd_[i];
       memset(outputVal, 0, sizeof outputVal);
       CoinConvertDouble(0, 0, lowerObjCoeffs_[i], outputVal); 
       line.append("\n" + columnName_[j] + " " + std::string(outputVal));
    }   
-   line.append("\n@VAREND\n");
+   line.append("\n@VARSEND\n");
    output->puts(line.c_str());
 
    line = "@CONSTRSBEGIN";
