c12d9788c5c47a089d092be0abc4031be947fba4
Ted Ralphs <ted@lehigh.edu>
2023-05-24 14:09:12 +0800

    Update MibSSolution.cpp

--- src/MibSSolution.cpp
+++ src/MibSSolution.cpp
@@ -152,7 +152,7 @@ MibSSolution::print(std::ostream& os) const
 		 os << "y[" << j << "] = " << values_[index] << std::endl;
 	     }
 	     else{
-		 os << "columnName[" << j << "] = " << values_[index] << std::endl;
+		 os << columnName[index] << " = " << values_[index] << std::endl;
 	     }
 	 }
       }
