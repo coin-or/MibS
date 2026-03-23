40e96528bf33c1c69e080ffa6d64412eb33745b5
Federico Battista <f.battista95@gmail.com>
2026-03-22 23:38:10 -0700

    Refactoring replication script integrating the new version

---
+++ scripts/replication/parameters.py
@@ -0,0 +1,184 @@
+# Script to replicate experiments of paper 
+# "Improving Directions in Mixed Integer Bilevel Linear Optimization"
+# Battista F. & Ted K. Ralphs
+# Last edited 2026
+
+import os
+from argparse import ArgumentDefaultsHelpFormatter
+from jsonargparse import ArgumentParser
+
+def _abs_path(path):
+    if path is None:
+        return None
+    
+    return os.path.abspath(os.path.expanduser(path))
+
+
+def _parseCliPairs(argument, cliValues):
+    """
+    Convert alternating key-value pairs CLI values into a dictionary.
+
+    Example input:
+        ["KEY1", "VALUE1", "KEY2", "VALUE2"]
+
+    Example output:
+        {
+            "KEY1": "VALUE1",
+            "KEY2": "VALUE2",
+        }
+    """
+    if cliValues is None:
+        return {}
+
+    if len(cliValues) % 2 != 0:
+        raise ValueError(f"Incorrect number of argument for {argument}. KEY VALUE pairs required.")
+
+    resultDict = {}
+    for i in range(0, len(cliValues), 2):
+        name = cliValues[i]
+        value = cliValues[i + 1]
+        resultDict[name] = _abs_path(value)
+
+    return resultDict
+
+
+class Parameters:
+    """
+    Shared CLI parser and parameter container.
+
+    The class defines the common command-line arguments used by the
+    replication scripts, parses them, and stores the resulting values in
+    `self.parameters`.
+
+    This class performs only syntactic validation of the arguments.
+    Semantic validation (e.g., checking that required arguments are present, 
+    or that file paths exist) should be performed separately after parsing 
+    by the user.
+    """
+
+    def __init__(self):
+        self.parser = ArgumentParser(
+            description="Command-line arguments for the replication scripts.",
+            formatter_class=ArgumentDefaultsHelpFormatter,
+        )
+        self.parameters = dict()
+
+        self.addArguments()
+
+    def __getitem__(self, key):
+        """
+        Return a parsed parameter by name.
+
+        This allows dictionary-style access such as `params["outputDir"]`
+        after `parse()` has been called.
+        """
+        return self.parameters[key]
+
+    def addArguments(self):
+        # run.py parameters
+        self.parser.add_argument(
+            "--binariesPath",
+            nargs="+",
+            default=None,
+            help="Executable binaries to use for the experiments, provided as VERSION PATH pairs.",
+        )
+
+        self.parser.add_argument(
+            "--instanceDirs",
+            nargs="+",
+            default=None,
+            help="Instance-set directories to process, provided as DATASET DIRECTORY pairs.",
+        )
+
+        self.parser.add_argument(
+            "--outputDir",
+            default=os.path.join(".", "results"),
+            help="Directory where experiment outputs, summaries, and generated files will be written.",
+        )
+
+        self.parser.add_argument(
+            "--writeParams",
+            action="store_true",
+            help="Write parameter files to <outputDir>/parameters instead of passing parameters on the command line.",
+        )
+
+        self.parser.add_argument(
+            "--testName",
+            default="mibs",
+            help="Job name to use when submitting experiments through qsub.",
+        )
+
+        self.parser.add_argument(
+            "--pbsFile",
+            default=None,
+            help="Path to the PBS submission script to use with qsub. If omitted, experiments are run locally.",
+        )
+
+        # make_plots.py parameters
+
+        self.parser.add_argument(
+            "--dataSets",
+            nargs="+",
+            default=None,
+            help="Dataset names to include when generating plots and summaries.",
+        )
+
+        self.parser.add_argument(
+            "--aggregateDatasets",
+            action="store_true",
+            help="Aggregate the selected datasets into a single combined summary and set of plots.",
+        )
+
+        self.parser.add_argument(
+            "--fileCsvIn",
+            default=None,
+            help="Path to an existing summary CSV file to read instead of parsing raw output files in <outputDir>.",
+        )
+
+
+    def parse(self):
+        """
+        Parse and syntactically validate the command-line arguments and store the results.
+
+        Raises:
+            ValueError: If required arguments are missing, or
+                if any key-value pair argument has an invalid format.
+        """
+        args = self.parser.parse_args()
+
+        try:
+            self.parameters["binariesPath"] = _parseCliPairs("--binariesPath", args.binariesPath)
+            self.parameters["instanceDirs"] = _parseCliPairs("--instanceDirs", args.instanceDirs)
+        except ValueError as exc:
+            self.parser.error(str(exc))
+
+        # Used in run.py, but not yet in make_plots.py 
+        self.parameters['versions'] = list(self.parameters['binariesPath'].keys())
+
+        for binName, path in self.parameters['binariesPath'].items():
+            self.parameters['binariesPath'][binName] = _abs_path(path)
+
+        for dataset, path in self.parameters['instanceDirs'].items():
+            self.parameters['instanceDirs'][dataset] = _abs_path(path)
+
+        self.parameters['outputDir'] = _abs_path(args.outputDir)
+        self.parameters['writeParams'] = args.writeParams
+        self.parameters['testName'] = args.testName
+        self.parameters['pbsFile'] = _abs_path(args.pbsFile)
+        self.parameters["dataSets"] = args.dataSets
+        self.parameters['aggregateDatasets'] = args.aggregateDatasets
+        self.parameters['fileCsvIn'] = _abs_path(args.fileCsvIn)
+
+        # TODO: read mibsParamsInputs and commonParams from CLI as well?
+        # Each configuration could be a file with parameters, and we can have
+        # a CLI argument for the directory where these files are stored.
+        # Then we can read them in and pass to the runExperiments function.
+
+if __name__ == "__main__":
+    # Example usage
+    params = Parameters()
+    params.parse()
+
+    print("Parsed parameters:")
+    for key, value in params.parameters.items():
+        print(f"{key}: {value}")
---
+++ scripts/replication/run.py
@@ -0,0 +1,192 @@
+# Script to replicate experiments of paper 
+# "Improving Directions in Mixed Integer Bilevel Linear Optimization"
+# Battista F. & Ted K. Ralphs
+# Last edited 2026
+
+import os
+import subprocess
+
+from parameters import Parameters
+
+# Import myrun
+from myrun import mibsParamsInputs, commonParams
+
+def writeParamsToFile(outDir, params):
+    """
+        Write MibS parameter for each scenario into a file
+    """
+    parampath = os.path.join(outDir, 'parameters')
+    if not os.path.exists(parampath):
+        os.mkdir(parampath)
+    
+    for scenario in params:
+        # for this scenario, make a parameter file and save to specified directory
+        paramsubpath1 = os.path.join(parampath, scenario)
+        if not os.path.exists(paramsubpath1):
+            os.mkdir(paramsubpath1)
+        paramfile = os.path.join(paramsubpath1, scenario + '.par')
+        with open(paramfile, 'w') as file:
+            for k, v in params[scenario].items():
+                file.write(k + ' ' + v + '\n')
+
+
+def runExperiments(instPaths, outDir, versions, params, pbsfile=None, testname='mibs'):
+    """
+        Use to run experiments on local machine or using qsub. 
+    """
+
+    # TODO: integrate gaps call for bounded rationality
+
+    # set up output directories
+    if not os.path.exists(outDir):
+        os.mkdir(outDir)
+    # use hierarchy:  outDir/version/param_scenario_name/testset_name/
+    for v in versions:
+        currpath = os.path.join(outDir, v)
+        if not os.path.exists(currpath):
+            os.mkdir(currpath)
+        for scenario in params:
+            currpath = os.path.join(outDir, v, scenario)
+            if not os.path.exists(currpath):
+                os.mkdir(currpath)
+            for testset in instPaths:
+                currsubpath1 = os.path.join(currpath, testset)
+                if not os.path.exists(currsubpath1):
+                    os.mkdir(currsubpath1)
+   
+    # if choose to write params into files
+    if writeParams:
+       writeParamsToFile(outDir, params)
+    
+    # Where parameter files are
+    parampath = os.path.join(outDir, 'parameters')
+
+    for v in versions:
+        print(f"Version: {v}")
+
+        exe = binariesPath[v]
+        for scenario in params:
+            print(f"   Scenario: {scenario}")
+            paramsubpath = os.path.join(parampath, scenario)     
+            for testset in instPaths:
+                paramfile = os.path.join(paramsubpath, scenario + '.par')
+                outsubpath = os.path.join(outDir, v, scenario, testset)
+                with os.scandir(instPaths[testset]) as inst_it:
+                    instances = [
+                        instance for instance in inst_it
+                        if instance.name.endswith('.mps') or instance.name.endswith('.mps.gz')
+                    ]
+
+                total_instances = len(instances)
+                print(f"      Dataset: {testset} ({total_instances} instances)")
+                for index, instance in enumerate(instances, start=1):
+                    remaining = total_instances - index
+
+                    if instance.name.endswith('.mps'):
+                        auxname = instance.path[:-4] + ".aux"
+                        outname = os.path.join(outsubpath, instance.name[:-4] + '.out')
+                        errname = os.path.join(outsubpath, instance.name[:-4] + ".err")
+                    elif instance.name.endswith('.mps.gz'):
+                        auxname = instance.path[:-7] + ".aux"
+                        outname = os.path.join(outsubpath, instance.name[:-7] + '.out')
+                        errname = os.path.join(outsubpath, instance.name[:-7] + ".err")
+                    else:
+                        # Something went wrong. Raise exception and exit.
+                        raise ValueError("Unexpected file extension!")
+
+                    print(
+                        "         "
+                        f"Instance: {instance.name} "
+                        f"({index}/{total_instances}, {remaining} remaining)"
+                    )
+
+                    # Check if output file already exists and is complete
+                    isComplete = False
+                    if os.path.exists(outname):
+                        with open(outname, 'r') as f:
+                            isComplete = "Number of problems (VF) solved" in f.read()
+                    
+                    isComplete = isComplete and (os.path.isfile(errname) and \
+                                           os.path.getsize(errname) == 0)
+                    
+                    if not isComplete:
+                        # prepare command for execution
+                        if pbsfile:
+                            # qsub 
+                            argList = ["qsub", "-v", 
+                                            "EXECUTABLE="+exe+","
+                                            +"INSTANCENAME="+instance.path+","
+                                            +"AUXNAME="+auxname+".aux"]
+                        else:
+                            # Local
+                            argList = [exe,
+                                        "-Alps_instance", instance.path,
+                                        "-MibS_auxiliaryInfoFile", auxname]
+
+                        # How do we pass parameters?      
+                        if writeParams:
+                            # Using file previously created
+                            if pbsfile:
+                                argList[-1] += ",PARAMARG=-param " + paramfile
+                            else:
+                                argList += ["-param", paramfile]
+                        else:
+                            # Using command line
+                            paramcmd = ' -'.join(' '.join(_) for _ in params[scenario].items())
+                            paramcmd = '' + paramcmd
+                            if pbsfile:
+                                argList[-1] += ",PARAMARG=" + paramcmd
+                            else:
+                                argList += paramcmd.split()
+                        
+                        # Run command and redirect output to outfile and errfile
+                        if pbsfile:
+                            argList += ["-o", outname,
+                                        "-e", errname,
+                                        "-N", testname,
+                                        pbsfile]
+                            subprocess.run(argList)
+                            print("            Submitted")
+
+                        else: 
+                            outfile = open(outname,'w')
+                            errfile = open(errname, 'w')
+                            subprocess.run(argList, stdout=outfile, stderr=errfile)
+                            outfile.close()
+                            errfile.close()
+                            print("            Complete")
+    
+                    else:
+                        print("            Skipping already complete output")
+
+if __name__ == "__main__":
+
+    # Update mibsParamsInputs with commonParams
+    for t in mibsParamsInputs:
+        mibsParamsInputs[t].update(commonParams)
+
+    # Read arguments from CLI
+    params = Parameters()
+    params.parse()
+
+    # Validate required arguments
+    if  not params['binariesPath'] or \
+        not params['instanceDirs']:
+          raise ValueError("Missing required arguments: --binariesPath, --instanceDirs, are required.")
+    
+    binariesPath = params['binariesPath']
+    instanceDirs = params['instanceDirs']
+    versions = params['versions']
+    outputDir = params['outputDir']
+    writeParams = params['writeParams']
+    testName = params['testName']
+    pbsFile = params['pbsFile']
+
+    ######################### Run Experimests #########################
+    runExperiments(instanceDirs, 
+                   outputDir, 
+                   versions, 
+                   mibsParamsInputs, 
+                   pbsfile=pbsFile, 
+                   testname=testName)
+    
